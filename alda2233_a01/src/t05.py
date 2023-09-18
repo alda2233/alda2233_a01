e53fb1fee9134fb104861c211fd45f59395056c0
mohamedaldakhil <mohamedaldakhil@10.84.99.243>
2023-09-18 08:50:06 -0400

    s

--- alda2233_a01/src/t05.py
+++ alda2233_a01/src/t05.py
@@ -5,7 +5,7 @@
 Author:  Mohamed
 ID:      169072233
 Email:   alda2233@mylaurier.ca
-__updated__ = "2023-09-13"
+__updated__ = "2023-09-18"
 -------------------------------------------------------
 """
 # Imports
@@ -31,3 +31,7 @@ Principal = float(input("Principal:"))
 Interest = float(input("Interest (%):"))
 Years = int(input("Number of years:"))
 compounded = int(input("Number of times interest compounded per year:"))
+
+AIntrest = Interest*100
+balance = Principal*(1+(AIntrest/compounded)**(compounded*Years))
+print(f"Balance: $ {balance}")
