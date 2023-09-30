5071eba5ca4ded31bcfd0a91d40754815007e3dc
mohamed223567 <131572320+mohamed223567@users.noreply.github.com>
2023-09-30 14:49:28 -0400

    a

--- alda2233_a01/src/t05.py
+++ alda2233_a01/src/t05.py
@@ -5,7 +5,7 @@
 Author:  Mohamed
 ID:      169072233
 Email:   alda2233@mylaurier.ca
-__updated__ = "2023-09-19"
+__updated__ = "2023-09-30"
 -------------------------------------------------------
 """
 # Imports
@@ -27,11 +27,11 @@ def func():
     """
 
 
-Principal = float(input("Principal:"))
-Interest = float(input("Interest (%):"))
-Years = int(input("Number of years:"))
-compounded = int(input("Number of times interest compounded per year:"))
+Principal = float(input("Principal: "))
+Interest = float(input("Interest (%): "))
+Years = int(input("Number of years: "))
+compounded = int(input("Number of times interest compounded per year: "))
 
-AIntrest = Interest*100
-balance = Principal*(1+(AIntrest/compounded)**(compounded*Years))
-print(f"Balance: $ {balance}")
+AInterest = Interest / 100  # Convert percentage to decimal
+balance = Principal * (1 + AInterest / compounded) ** (compounded * Years)
+print(f"Balance: ${balance:.10f}")
